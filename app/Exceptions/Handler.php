04aae5d7539bb69dddbcc0624b012f0d6e755abe
twiceM <mohamed98mahmoud2016@gmail.com>
2023-09-20 19:37:01 +0300

    Init Login and api responses strctures

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use App\Http\Responses\ApiResponse;
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,8 +27,26 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Exception|\Throwable $e)
+    {
+        if ($request->expectsJson()) {
+
+            // Handle JSON requests with your custom JSON responses
+            if ($e instanceof ValidationException) {
+                return ApiResponse::errorResponse($e->getMessage(), Response::HTTP_UNPROCESSABLE_ENTITY,$e->errors());
+            } else  {
+                return ApiResponse::errorResponse($e->getMessage(), Response::HTTP_NOT_FOUND);
+            }
+
+
+        }
+
+        return parent::render($request, $e);
     }
 }
---
+++ app/Http/Controllers/ApiControllers/Auth/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\ApiControllers\Auth;
+
+
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\UserResource;
+use App\Http\Responses\ApiResponse;
+use App\Models\User;
+use \Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+
+    public function __invoke(LoginRequest $request)
+    {
+
+        // Get user email
+        $user = User::where("email","=",$request->get("email"))->first();
+
+        // Check if user email is found
+        if ($user == null) {
+            return ApiResponse::errorResponse("Email or Password is not right");
+        }
+
+        // Check password of the user
+        if (!Hash::check($request->get("password"),$user->password)) {
+            return ApiResponse::errorResponse("Email or Password is not right");
+        }
+
+
+        $token = $user->createToken("Web browser");
+
+        $user->token = $token->plainTextToken;
+
+        return ApiResponse::successResponse("Login Successfully",200,new UserResource($user));
+    }
+}
